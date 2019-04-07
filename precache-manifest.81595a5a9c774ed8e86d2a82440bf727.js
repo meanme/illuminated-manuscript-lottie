c1a06f86b0d2581cb9da910409f760e2ccce49ee
Matteo <mmarinucci@netflix.com>
2019-04-07 16:55:23 -0700

    Updates

---
+++ precache-manifest.81595a5a9c774ed8e86d2a82440bf727.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7402dfc7ff63a59c4f3a",
+    "url": "/illuminated-manuscript-lottie/static/js/runtime~main.bd44a2b7.js"
+  },
+  {
+    "revision": "10eb502447a10c079afc",
+    "url": "/illuminated-manuscript-lottie/static/js/main.2cfd2b76.chunk.js"
+  },
+  {
+    "revision": "d08faf9d26134c8184c4",
+    "url": "/illuminated-manuscript-lottie/static/js/2.1376dd57.chunk.js"
+  },
+  {
+    "revision": "10eb502447a10c079afc",
+    "url": "/illuminated-manuscript-lottie/static/css/main.05be80e5.chunk.css"
+  },
+  {
+    "revision": "af2c6dd66053e2ec5956578dfc2634ef",
+    "url": "/illuminated-manuscript-lottie/index.html"
+  }
+];
